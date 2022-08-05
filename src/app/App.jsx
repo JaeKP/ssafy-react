65f63c43ff93e7d52a1dba1d66f1110e151baddf
yamoo9 <yamoo9@naver.com>
2022-08-06 08:48:28 +0900

    config. React + Vite Template

---
+++ src/app/App.jsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+import reactLogo from '@/assets/react.svg';
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      <h1>React + Vite</h1>
+      <img src={reactLogo} alt="React" height={100} />
+    </div>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTest.js',
+    css: true,
+  },
+});
