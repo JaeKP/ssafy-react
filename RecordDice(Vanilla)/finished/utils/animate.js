2f01e77068f4254f1e51a174ac5292d6b6fa5bdf
yamoo9 <yamoo9@naver.com>
2022-08-09 14:32:44 +0900

    add. Record Dice (Vanilla version)

---
+++ RecordDice(Vanilla)/finished/utils/animate.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+const animate = (() => {
+  let clearId;
+  return function recursion(callback, FPS = 60) {
+    callback?.();
+    clearId = setTimeout(recursion.bind(null, callback, FPS), 1000 / FPS);
+    return () => clearTimeout(clearId);
+  };
+})();
---
+++ RecordDice(Vanilla)/finished/utils/getRandom.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+function getRandom(n) {
+  return Math.round(Math.random() * n);
+}
+
+function getRandomMinMax(min = 0, max = 10) {
+  return getRandom(max - min) + min;
+}
